d9af35e9dca40212206681210d39324ad1401c07
tanya574033 <56290920+tanya574033@users.noreply.github.com>
2024-12-03 16:02:46 +0700

    Update ScreenRecordingDetector
    Update ScreenRecordingDetector

--- src/ios/ScreenRecordingDetector.m
+++ src/ios/ScreenRecordingDetector.m
@@ -2,21 +2,21 @@
 //  ScreenRecordingDetector.m
 //  ScreenCaptureDetector
 //
-//
-
+ 
 #import "ScreenRecordingDetector.h"
-float const kScreenRecordingDetectorTimerInterval = 1.0;
+ 
+float const kScreenRecordingDetectorTimerInterval = 1; // Reduced interval for faster detection
 NSString *kScreenRecordingDetectorRecordingStatusChangedNotification = @"kScreenRecordingDetectorRecordingStatusChangedNotification";
-
+ 
 @interface ScreenRecordingDetector()
-
+ 
 @property BOOL lastRecordingState;
 @property NSTimer *timer;
-
+ 
 @end
+ 
 @implementation ScreenRecordingDetector
-
-
+ 
 + (instancetype)sharedInstance {
     static ScreenRecordingDetector *sharedInstance = nil;
     static dispatch_once_t onceToken;
@@ -25,54 +25,62 @@ NSString *kScreenRecordingDetectorRecordingStatusChangedNotification = @"kScreen
     });
     return sharedInstance;
 }
+ 
 - (id)init {
     if (self = [super init]) {
-        // do some init stuff here..
-        self.lastRecordingState = NO; // initially the recording state is 'NO'. This is the default state.
+        // Initialize properties
+        self.lastRecordingState = NO; // Default state: not recording
         self.timer = NULL;
     }
     return self;
 }
+ 
 - (BOOL)isRecording {
-    #if TARGET_IPHONE_SIMULATOR
-        return NO;
-    #endif
+#if TARGET_IPHONE_SIMULATOR
+    return NO; // Screen recording detection does not work on the simulator
+#endif
     for (UIScreen *screen in UIScreen.screens) {
         if ([screen respondsToSelector:@selector(isCaptured)]) {
-            // iOS 11+ has isCaptured method.
+            // iOS 11+ has isCaptured method
             if ([screen performSelector:@selector(isCaptured)]) {
-                return YES; // screen capture is active
+                return YES; // Screen capture is active
             } else if (screen.mirroredScreen) {
-                return YES; // mirroring is active
+                return YES; // Screen mirroring is active
             }
         } else {
             // iOS version below 11.0
-            if (screen.mirroredScreen)
+            if (screen.mirroredScreen) {
                 return YES;
+            }
         }
     }
     return NO;
 }
+ 
 + (void)triggerDetectorTimer {
-
     ScreenRecordingDetector *detector = [ScreenRecordingDetector sharedInstance];
     if (detector.timer) {
         [self stopDetectorTimer];
     }
+    // Immediate check for recording state
+    [detector checkCurrentRecordingStatus:nil];
+    // Start the timer for periodic checks
     detector.timer = [NSTimer scheduledTimerWithTimeInterval:kScreenRecordingDetectorTimerInterval
-                                                           target:detector
-                                                         selector:@selector(checkCurrentRecordingStatus:)
-                                                         userInfo:nil
-                                                          repeats:YES];
+                                                      target:detector
+                                                    selector:@selector(checkCurrentRecordingStatus:)
+                                                    userInfo:nil
+                                                     repeats:YES];
 }
+ 
 - (void)checkCurrentRecordingStatus:(NSTimer *)timer {
     BOOL isRecording = [self isRecording];
     if (isRecording != self.lastRecordingState) {
         NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-        [center postNotificationName: kScreenRecordingDetectorRecordingStatusChangedNotification object:nil];
+        [center postNotificationName:kScreenRecordingDetectorRecordingStatusChangedNotification object:nil];
     }
     self.lastRecordingState = isRecording;
 }
+ 
 + (void)stopDetectorTimer {
     ScreenRecordingDetector *detector = [ScreenRecordingDetector sharedInstance];
     if (detector.timer) {
@@ -80,4 +88,5 @@ NSString *kScreenRecordingDetectorRecordingStatusChangedNotification = @"kScreen
         detector.timer = NULL;
     }
 }
+ 
 @end
