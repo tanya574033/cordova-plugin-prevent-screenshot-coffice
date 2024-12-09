5c3fd67270117a05bd0cc5f6828b26e1f428e8fb
tanya574033 <56290920+tanya574033@users.noreply.github.com>
2024-12-09 14:38:25 +0700

    Update ScreenshotBlocker.m
    fix capture screenshot

--- src/ios/ScreenshotBlocker.m
+++ src/ios/ScreenshotBlocker.m
@@ -1,118 +1,191 @@
+/**
+ * ScreenshotBlocker.m
+ * Created by Tommy-Carlos Williams on 18/07/2014
+ * Copyright (c) 2014 Tommy-Carlos Williams. All rights reserved.
+ * MIT Licensed
+ */
+#import "AppDelegate.h"
+#import "MainViewController.h"
 #import "ScreenshotBlocker.h"
-@interface ScreenshotBlocker() {
-    CDVInvokedUrlCommand * _eventCommand;
-}
-@end
+
+static UIImageView *imageView;
 
 @implementation ScreenshotBlocker
-UIImageView* cover;
-- (void)pluginInitialize {
-    NSLog(@"Starting ScreenshotBlocker plugin");
 
-    [[NSNotificationCenter defaultCenter]addObserver:self
-                                            selector:@selector(appDidBecomeActive)
-                                                name:UIApplicationDidBecomeActiveNotification
-                                              object:nil];
-    [[NSNotificationCenter defaultCenter]addObserver:self
-                                            selector:@selector(applicationWillResignActive)
-                                                name:UIApplicationWillResignActiveNotification
-                                              object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(tookScreeshot)
-                                                 name:UIApplicationUserDidTakeScreenshotNotification
-                                               object:nil];
-
-    [[NSNotificationCenter defaultCenter]addObserver:self
-                                            selector:@selector(goingBackground)
-                                                name:UIApplicationWillResignActiveNotification
-                                              object:nil];
-
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(screenCaptureStatusChanged)
-                                                 name:kScreenRecordingDetectorRecordingStatusChangedNotification
-                                               object:nil];
-
-    /*
-     userDidTakeScreenshotNotification
-     */
-
-}
-
-- (void)enable:(CDVInvokedUrlCommand *)command
+- (void)pluginInitialize
 {
-    CDVPluginResult* pluginResult = nil;
-    NSLog(@"Abilita observers");
-    /*
-     [[NSNotificationCenter defaultCenter]addObserver:self
-     selector:@selector(appDidBecomeActive)
-     name:UIApplicationDidBecomeActiveNotification
-     object:nil];
-     [[NSNotificationCenter defaultCenter]addObserver:self
-     selector:@selector(applicationWillResignActive)
-     name:UIApplicationWillResignActiveNotification
-     object:nil];
-     [[NSNotificationCenter defaultCenter] addObserver:self
-     selector:@selector(screenCaptureStatusChanged)
-     name:kScreenRecordingDetectorRecordingStatusChangedNotification
-     object:nil];
-     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     */
-}
--(void)listen:(CDVInvokedUrlCommand*)command {
-    _eventCommand = command;
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppDidBecomeActive:)
+                                               name:UIApplicationDidBecomeActiveNotification object:nil];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppWillResignActive:)
+                                               name:UIApplicationWillResignActiveNotification object:nil];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppDidFinishLaunching:)
+                                                 name:UIApplicationDidFinishLaunchingNotification object:nil];
 }
 
+- (void)onAppDidBecomeActive:(UIApplication *)application
+{
+  if (imageView == NULL) {
+    self.viewController.view.window.hidden = NO;
+  } else {
+    [imageView removeFromSuperview];
+  }
+}
 
--(void) goingBackground {
-    NSLog(@"Me la scattion in bck");
-    if(_eventCommand!=nil) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"background"];
-        [pluginResult setKeepCallbackAsBool:YES];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:_eventCommand.callbackId];
+- (void)onAppWillResignActive:(UIApplication *)application
+{
+  CDVViewController *vc = (CDVViewController*)self.viewController;
+  NSString *imgName = [self getImageName:self.viewController.interfaceOrientation delegate:(id<CDVScreenOrientationDelegate>)vc device:[self getCurrentDevice]];
+  UIImage *splash = [UIImage imageNamed:imgName];
+  if (splash == NULL) {
+    imageView = NULL;
+    self.viewController.view.window.hidden = YES;
+  } else {
+    imageView = [[UIImageView alloc]initWithFrame:[self.viewController.view bounds]];
+    [imageView setImage:splash];
+    
+    #ifdef __CORDOVA_4_0_0
+        [[UIApplication sharedApplication].keyWindow addSubview:imageView];
+    #else
+        [self.viewController.view addSubview:imageView];
+    #endif
+  }
+}
+
+// Code below borrowed from the CDV splashscreen plugin @ https://github.com/apache/cordova-plugin-splashscreen
+// Made some adjustments though, becuase landscape splashscreens are not available for iphone < 6 plus
+- (CDV_iOSDevice) getCurrentDevice
+{
+  CDV_iOSDevice device;
+  
+  UIScreen* mainScreen = [UIScreen mainScreen];
+  CGFloat mainScreenHeight = mainScreen.bounds.size.height;
+  CGFloat mainScreenWidth = mainScreen.bounds.size.width;
+  
+  int limit = MAX(mainScreenHeight,mainScreenWidth);
+  
+  device.iPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+  device.iPhone = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone);
+  device.retina = ([mainScreen scale] == 2.0);
+  device.iPhone4 = (device.iPhone && limit == 480.0);
+  device.iPhone5 = (device.iPhone && limit == 568.0);
+  // note these below is not a true device detect, for example if you are on an
+  // iPhone 6/6+ but the app is scaled it will prob set iPhone5 as true, but
+  // this is appropriate for detecting the runtime screen environment
+  device.iPhone6 = (device.iPhone && limit == 667.0);
+  device.iPhone6Plus = (device.iPhone && limit == 736.0);
+  
+  return device;
+}
+
+- (NSString*)getImageName:(UIInterfaceOrientation)currentOrientation delegate:(id<CDVScreenOrientationDelegate>)orientationDelegate device:(CDV_iOSDevice)device
+{
+  // Use UILaunchImageFile if specified in plist.  Otherwise, use Default.
+  NSString* imageName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UILaunchImageFile"];
+  
+  NSUInteger supportedOrientations = [orientationDelegate supportedInterfaceOrientations];
+  
+  // Checks to see if the developer has locked the orientation to use only one of Portrait or Landscape
+  BOOL supportsLandscape = (supportedOrientations & UIInterfaceOrientationMaskLandscape);
+  BOOL supportsPortrait = (supportedOrientations & UIInterfaceOrientationMaskPortrait || supportedOrientations & UIInterfaceOrientationMaskPortraitUpsideDown);
+  // this means there are no mixed orientations in there
+  BOOL isOrientationLocked = !(supportsPortrait && supportsLandscape);
+  
+  if (imageName) {
+    imageName = [imageName stringByDeletingPathExtension];
+  } else {
+    imageName = @"Default";
+  }
+
+  // Add Asset Catalog specific prefixes
+  if ([imageName isEqualToString:@"LaunchImage"])
+  {
+    if(device.iPhone4 || device.iPhone5 || device.iPad) {
+      imageName = [imageName stringByAppendingString:@"-700"];
+    } else if(device.iPhone6) {
+      imageName = [imageName stringByAppendingString:@"-800"];
+    } else if(device.iPhone6Plus) {
+      imageName = [imageName stringByAppendingString:@"-800"];
+      if (currentOrientation == UIInterfaceOrientationPortrait || currentOrientation == UIInterfaceOrientationPortraitUpsideDown) {
+        imageName = [imageName stringByAppendingString:@"-Portrait"];
+      }
     }
-}
--(void)tookScreeshot {
-    NSLog(@"fatta la foto?");
-    if(_eventCommand!=nil) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"tookScreenshot"];
-        [pluginResult setKeepCallbackAsBool:YES];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:_eventCommand.callbackId];
-    }
-
-}
-
--(void)setupView {
-    BOOL isCaptured = [[UIScreen mainScreen] isCaptured];
-    NSLog(@"Is screen captured? %@", (isCaptured?@"SI":@"NO"));
-
-    if ([[ScreenRecordingDetector sharedInstance] isRecording]) {
-        [self webView].alpha = 0.f;
-        NSLog(@"Registro o prendo screenshots");
+  }
+  
+  BOOL isLandscape = supportsLandscape &&
+  (currentOrientation == UIInterfaceOrientationLandscapeLeft || currentOrientation == UIInterfaceOrientationLandscapeRight);
+  
+  if (device.iPhone5) { // does not support landscape
+    imageName = isLandscape ? nil : [imageName stringByAppendingString:@"-568h"];
+  } else if (device.iPhone6) { // does not support landscape
+    imageName = isLandscape ? nil : [imageName stringByAppendingString:@"-667h"];
+  } else if (device.iPhone6Plus) { // supports landscape
+    if (isOrientationLocked) {
+      imageName = [imageName stringByAppendingString:(supportsLandscape ? @"-Landscape" : @"")];
     } else {
-        [self webView].alpha = 1.f;
-        NSLog(@"Non registro");
-
+      switch (currentOrientation) {
+        case UIInterfaceOrientationLandscapeLeft:
+        case UIInterfaceOrientationLandscapeRight:
+          imageName = [imageName stringByAppendingString:@"-Landscape"];
+          break;
+        default:
+          break;
+      }
     }
+    imageName = [imageName stringByAppendingString:@"-736h"];
+    
+  } else if (device.iPad) { // supports landscape
+    if (isOrientationLocked) {
+      imageName = [imageName stringByAppendingString:(supportsLandscape ? @"-Landscape" : @"-Portrait")];
+    } else {
+      switch (currentOrientation) {
+        case UIInterfaceOrientationLandscapeLeft:
+        case UIInterfaceOrientationLandscapeRight:
+          imageName = [imageName stringByAppendingString:@"-Landscape"];
+          break;
+          
+        case UIInterfaceOrientationPortrait:
+        case UIInterfaceOrientationPortraitUpsideDown:
+        default:
+          imageName = [imageName stringByAppendingString:@"-Portrait"];
+          break;
+      }
+    }
+  }
+  
+  return imageName;
 }
 
--(void)appDidBecomeActive {
-    [ScreenRecordingDetector triggerDetectorTimer];
-    if(cover!=nil) {
-        [cover removeFromSuperview];
-        cover = nil;
-    }
+- (void)onAppDidFinishLaunching:(NSNotification *)notification {
+    // Este método será chamado quando o aplicativo for inicializado
+    NSLog(@"O aplicativo foi inicializado!");
+    
+    // Aqui você pode chamar a função makeSecure
+    [self makeSecure];
 }
--(void)applicationWillResignActive {
-    [ScreenRecordingDetector stopDetectorTimer];
-    if(cover == nil) {
-        cover = [[UIImageView alloc] initWithFrame:[self.webView frame]];
-        cover.backgroundColor = [UIColor blackColor];
-        [self.webView addSubview:cover];
-    }
-}
--(void)screenCaptureStatusChanged {
-    [self setupView];
+
+- (void) makeSecure {
+    // Código para chamar makeSecure
+    UIWindow *window = [[[[UIApplication sharedApplication] delegate] window] rootViewController].view.window;
+
+    UITextField *field = [[UITextField alloc] init];
+
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, field.frame.size.width, field.frame.size.height)];
+
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"whiteImage"]];
+    image.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+
+    field.secureTextEntry = YES;
+
+    [window addSubview:field];
+    [view addSubview:image];
+
+    [window.layer.superlayer addSublayer:field.layer];
+    [[field.layer.sublayers lastObject] addSublayer:window.layer];
+
+    field.leftView = view;
+    field.leftViewMode = UITextFieldViewModeAlways;
 }
 
 
