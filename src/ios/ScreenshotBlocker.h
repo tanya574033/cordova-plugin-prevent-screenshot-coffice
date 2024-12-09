d818fad872a9c6577bb7f4a8a30ebc1cdb217281
tanya574033 <56290920+tanya574033@users.noreply.github.com>
2024-12-09 14:39:05 +0700

    Update ScreenshotBlocker.h
    fix capture shotscreen

--- src/ios/ScreenshotBlocker.h
+++ src/ios/ScreenshotBlocker.h
@@ -1,10 +1,28 @@
 #import <Cordova/CDV.h>
-#import "ScreenRecordingDetector.h"
+#import <Cordova/CDVPlugin.h>
 
+typedef struct {
+  BOOL iPhone;
+  BOOL iPad;
+  BOOL iPhone4;
+  BOOL iPhone5;
+  BOOL iPhone6;
+  BOOL iPhone6Plus;
+  BOOL iPhoneX;
+  BOOL iPhoneXS;
+  BOOL iPhoneXR;
+  BOOL iPhone11;
+  BOOL iPhone15Pro;
+  BOOL iPhone12;
+  BOOL iPhone12ProMax;
+  BOOL iPhone13;
+  BOOL iPhone13ProMax;
+  BOOL iPadPro11;
+  BOOL iPadPro12_9;
+  BOOL retina;
+  
+} CDV_iOSDevice;
 
 @interface ScreenshotBlocker : CDVPlugin
-
-- (void)enable:(CDVInvokedUrlCommand*)command;
--(void)listen:(CDVInvokedUrlCommand*)command;
 
 @end
